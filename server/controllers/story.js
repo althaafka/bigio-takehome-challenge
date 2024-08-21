262b752ef194beb1a8ae09de972b692dd82ab91f
althaafka <althaafka004@gmail.com>
2024-08-21 21:44:43 +0700

    feat (be): add endpoint for add story

--- server/controllers/story.js
+++ server/controllers/story.js
@@ -10,3 +10,29 @@ exports.getStories = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+
+exports.addStory = async (req, res) => {
+    const { title, author, synopsis, category, coverImage, tags, status, chapters } = req.body;
+
+    try {
+        const newStory = await Story.create(
+            {
+                title,
+                author,
+                synopsis,
+                category,
+                coverImage,
+                tags,
+                status,
+                chapters,
+            },
+            {
+                include: [{ model: Chapter, as: 'chapters' }],
+            }
+        );
+
+        res.status(201).json(newStory);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
--- server/routes/story.js
+++ server/routes/story.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const storyController = require('../controllers/story');
 
 router.get('/', storyController.getStories);
-
+router.post('/', storyController.addStory);
 
 module.exports = router;
